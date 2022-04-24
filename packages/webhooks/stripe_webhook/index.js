549deaa74a0a25304dffd7447e1cbe9f1ef64b3b
Davi DeBarros <ddebarros@digitalocean.com>
2022-04-23 21:15:23 -0400

    Test

--- packages/webhooks/stripe_webhook/index.js
+++ packages/webhooks/stripe_webhook/index.js
@@ -7,17 +7,19 @@ const stripe = require("stripe")(STRIPE_SECRET_KEY);
 async function main(args) {
   const sig = args.__ow_headers['stripe-signature'];
 
+  console.log('PAYLOAD => ', payload)
+  console.log('ARGS => ', args)
+
   /**
    * To validate the payload signature we must use the 
    * exact request body sent from the stripe API request. 
    * To prevent the platform from parsing the request content, 
    * we must set "annotations.raw-http=true" in the functions configuration
    */
-  const payload = new Buffer.from(args.__ow_body, 'base64')
+  const payload = new Buffer.from(args.__ow_body ?? '', 'base64')
 
-  console.log('PAYLOAD => ', payload)
-  console.log('ARGS => ', args)
-  
+
+
   let event = null;
   try {
     event = stripe.webhooks.constructEvent(payload, sig, STRIPE_WEBHOOK_ENDPOINT_SECRET);
