b22a3ddf6bac4c14d5ff5e125410c59fbaf87729
Davi DeBarros <ddebarros@digitalocean.com>
2022-04-23 20:45:08 -0400

    Updates redirect url

--- src/CheckoutForm.jsx
+++ src/CheckoutForm.jsx
@@ -58,7 +58,7 @@ export default function CheckoutForm() {
       elements,
       confirmParams: {
         // Make sure to change this to your payment completion page
-        return_url: window.location.host,
+        return_url: window.location.origin,
       },
     });
 
